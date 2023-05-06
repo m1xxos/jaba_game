4d9c451f28af5c38fe119303bf3465cc6472ebf4
m1xxos <66390094+m1xxos@users.noreply.github.com>
2023-05-06 15:29:49 +0300

    cards

--- Assets/CardScript.cs
+++ Assets/CardScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,32 @@ public class CardScript : MonoBehaviour
 {
     public string direction;
     public string color;
+    public bool hasBeenPlayed;
+    public int handIndex;
+    private GameManagerScript gm;
+
+
+    private void OnMouseDown()
+    {
+        if (hasBeenPlayed == false)
+        {
+            transform.position += Vector3.up * 20;
+            hasBeenPlayed = true;
+            gm.avalibaleCardSlots[handIndex] = true;
+            Invoke("MoveToDiscardPile", 2f);
+        }
+    }
+
+    void MoveToDiscardPile()
+    {
+        gm.discard.Add(this);
+        gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = FindObjectOfType<GameManagerScript>();
     }
 
     // Update is called once per frame
--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -6,7 +6,8 @@ using Random = UnityEngine.Random;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public List<GameObject> deck = new List<GameObject>();
+    public List<CardScript> deck = new List<CardScript>();
+    public List<CardScript> discard = new List<CardScript>();
     public Transform[] cardSlots;
     public bool[] avalibaleCardSlots;
     
@@ -14,18 +15,34 @@ public class GameManagerScript : MonoBehaviour
     {
         if (deck.Count >= 1)
         {
-            GameObject randCard = deck[Random.Range(0, deck.Count)];
+            CardScript randCard = deck[Random.Range(0, deck.Count)];
             for (int i = 0; i < avalibaleCardSlots.Length; i++)
             {
                 if (avalibaleCardSlots[i] == true)
                 {
                     randCard.gameObject.SetActive(true);
+                    randCard.handIndex = i;
+                    
                     randCard.transform.position = cardSlots[i].position;
+                    randCard.hasBeenPlayed = false;
+
                     avalibaleCardSlots[i] = false;
                     deck.Remove(randCard);
                     return;
                 }
             }
+        }
+    }
+
+    public void Shuffle()
+    {
+        if (discard.Count >= 1)
+        {
+            foreach (CardScript card in discard)
+            {
+                deck.Add(card);
+            }
+            discard.Clear();
         }
     }
 }
